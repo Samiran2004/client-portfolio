7939cf6a2a10980d1432300c89429e766d62cca8
subrata-code <bagsubrata193@gmail.com>
2025-05-09 12:41:50 +0530

    Add there about section with-out responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
-
+import AboutSection from "../components/AboutSection"; // Import AboutSection
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <AboutSection /> {/* Add AboutSection here */}
     </>
-    
   );
 };
 
